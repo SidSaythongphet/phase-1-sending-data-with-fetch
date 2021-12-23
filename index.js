0c1cb04193d08ae3634e9ef24da177a78c26d4d6
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-23 17:23:19 -0500

    commit tree c39bbb2e04d556808d2689d19dce7a7082fedcae

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+// function submitData(userName, userEmail) {
+//     return fetch(`http://localhost:3000/users`, {
+//         method: `POST`,
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//         },
+//         body: JSON.stringify({
+//             name:`${userName}`, 
+//             email:`${userEmail}`,
+//         })
+//     }) 
+// }
+
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        }) 
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(obj => {
+        document.querySelector('body').append(obj.id)
+    })
+    .catch(error => {
+        document.querySelector('body').append("Unauthorized Access")
+    })
+}
+
+submitData('Bob', 'bob@bob.com')
